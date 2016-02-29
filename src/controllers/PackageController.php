cf7e67d5721a801ee3b4a78c5cf335425dc79eb9
Ronalds Sovas <sovas@greenbox.lv>
2016-02-29 17:42:13 +0200

    dispatche command update

--- src/controllers/PackageController.php
+++ src/controllers/PackageController.php
@@ -1,6 +1,6 @@
 <?php namespace Shivergard\Crud;
 
-use Illuminate\Foundation\Bus\DispatchesCommands;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
